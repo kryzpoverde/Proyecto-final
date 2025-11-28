f246075fff665db4bdd7bd20b3fad24b2bbc2a27
kryzpoverde <gseguliz@uc.cl>
2025-11-28 00:26:41 -0300

    CÓDIGO

---
+++ code/01_cleaning.R
@@ -0,0 +1,13 @@
+# Proyecto final – Visualización de datos
+# 01_cleaning.R
+# Limpieza inicial de datos 
+# Autora: Golondrina Segura
+
+# Librerías
+library(tidyverse)
+
+# Leer datos raw
+spells_raw <- read_csv("data/raw/spells.csv")
+monsters_raw <- read_csv("data/raw/monsters.csv")
+
+# Aquí irá la limpieza más adelante
