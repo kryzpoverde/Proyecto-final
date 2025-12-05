c29e9d507cd5ffec02a51b60b9e464f3fec7c0c7
kryzpoverde <gseguliz@uc.cl>
2025-12-04 22:25:31 -0300

    CÓDIGOS ACTUALIZADOS

--- code/02_exploration.R
+++ code/02_exploration.R
@@ -1,16 +1,68 @@
-# 02_exploration.R
-# Análisis exploratorio de los datos de spells y monsters
 # Proyecto final – Visualización de datos
+# 02_exploration.R
+# Análisis exploratorio de los datos
 # Autora: Golondrina Segura
+
+# Cargar librerías
 
 library(tidyverse)
 
-# Leer datos procesados (cuando los tengamos)
-# Por ahora esto es solo la estructura
-# spells <- read_csv("data/procesados/spells_clean.csv")
-# monsters <- read_csv("data/procesados/monsters_clean.csv")
+# Leer datos procesados
 
-# Aquí después vamos a:
-# - hacer preguntas exploratorias
-# - calcular resúmenes
-# - crear gráficos exploratorios
+spells <- read_csv("data/procesados/spells_clean.csv")
+monsters <- read_csv("data/procesados/monsters_clean.csv")
+
+# Observar los datos
+
+glimpse(spells)
+glimpse(monsters) #existen datos NA en filas de resistencias, vulnerabilidades e inmunidades, pero estos no son 
+                  #datos faltantes por lo que no se eliminan, se interpretan como que el monstruo no posee nada de este carácter.
+
+# Vamos a empezar revisando cuántos hechizos hay por nivel
+
+spells_por_nivel <- spells %>%
+  count(level, name = "cantidad_hechizos")
+spells_por_nivel
+
+# Graficar resultado
+
+ggplot(spells_por_nivel, aes(x = level, y = cantidad_hechizos)) +
+  geom_col(fill = "#9333FF", alpha = 0.6) +
+  scale_x_continuous(breaks = 0:9) +  
+  scale_y_continuous(breaks = seq(0, 60, by = 5)) +  
+  labs(
+    title = "Cantidad de hechizos por nivel",
+    x = "Nivel del hechizo",
+    y = "Número de hechizos"
+  ) +
+  theme_classic()
+
+#Ahora revisamos cantidad de montruos por CR (challenge rating)
+#Para esto, vamos a agregar una columna que agrupa
+#los monstruos según su CR (noob, intermedio, avanzado, épico y legendario)
+
+monsters <- monsters %>%
+  mutate(cr_group = case_when(
+    cr <= 2 ~ "Noob",
+    cr <= 6 ~ "Intermedio",
+    cr <= 10 ~ "Avanzado",
+    cr <= 16 ~ "Épico",
+    TRUE ~ "Legendario"
+  ))
+
+#Ahora graficamos el resultado
+
+ggplot(monsters, aes(x = factor(cr_group,
+                                levels = c("Noob", "Intermedio", "Avanzado", "Épico", "Legendario")))) +
+  geom_bar(fill = "#FF8F78", alpha = 0.8) +
+  scale_y_continuous(breaks = seq(0, max(table(monsters$cr_group)), by = 50)) +
+  labs(
+    title = "Distribución de monstruos por nivel de amenaza",
+    x = "Nivel de amenaza",
+    y = "Cantidad de monstruos"
+  ) +
+  theme_classic()
+
+
+
+
--- code/03_plots.R
+++ code/03_plots.R
@@ -1,14 +1,4 @@
-# 03_plots.R
-# Gráficos finales para el sitio web en Quarto
 # Proyecto final – Visualización de datos
+# 03_plots.R
+# Gráficos finales
 # Autora: Golondrina Segura
-
-library(tidyverse)
-
-# Leer datos procesados (cuando estén listos)
-# spells <- read_csv("data/procesados/spells_clean.csv")
-# monsters <- read_csv("data/procesados/monsters_clean.csv")
-
-# Aquí vamos a guardar el código de:
-# - gráficos finales
-# - figuras que se exportan a /figures
